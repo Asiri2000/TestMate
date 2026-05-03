d198274048a0e30e727799e14ecb4bc473e1b74e
Asiri2000 <149108334+Asiri2000@users.noreply.github.com>
2026-05-03 08:53:20 +0530

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <>
-      <h1>Welcome to the Frontend!</h1>
+      <h1>Welcome to the Frontend! </h1>
+
+      <p>This is the frontend of the TestMate application. Here, you can interact with the user interface and access various features of the application.</p>
     </>
   )
 }
