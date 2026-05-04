6802f5bf6aa2e34fc42405d988e49c902b0bb8da
Asiri2000 <149108334+Asiri2000@users.noreply.github.com>
2026-05-04 11:19:55 +0530

    updated

--- front-end/src/CandidateProfile.jsx
+++ front-end/src/CandidateProfile.jsx
@@ -1,9 +1,14 @@
 export const CandidateProfile = () => {
 
+ const name = "Peter Parker";
+ const profession = "Software Engineer";
+ const available = true;
+
      return(
           <div>
-              <h3>Peter parker</h3>
-              <h4>Software Engineer</h4>
+              <h3>{name}</h3>
+              <h4>{profession}</h4>
+              <h5> {available ? "Available" : "Not Available"} </h5>
 
           </div>
 
